d3fdd533e8e961c357741570bb308615b8ddd955
vjmathus <vjmathus786@gmail.com>
2022-06-28 08:57:31 +0530

    my try

--- hello.java
+++ hello.java
@@ -1,8 +1,10 @@
 //hello
-public class hello{
-    
- public static void main(String[] args) {
-System.out.println("nan");
+public class hello {
 
- }       
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 54;
+        System.out.println(a + b);
+        System.out.println("vijay");
+    }
 }
