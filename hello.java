c552d57c096fa99edee8dd2a7bc145c56fb228f3
vjmathus <vjmathus786@gmail.com>
2022-06-28 09:49:44 +0530

    new changes

--- hello.java
+++ hello.java
@@ -5,6 +5,14 @@ public class hello {
         int a = 12;
         int b = 54;
         System.out.println(a + b);
+        System.out.println("mojo");
         System.out.println("vijay");
+        String c="vijay";
+        String d="";
+        for(int i=c.length()-1;i>=0;i--){
+            d=d+c.charAt(i);
+
+        }
+        System.out.println("Reversing string:"+d);
     }
 }
